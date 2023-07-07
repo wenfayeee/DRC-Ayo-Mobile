36bd0429611b82683bfaef4f6e96ce0ec8c70ae9
wenfayeee <munnfayee@gmail.com>
2023-07-07 17:26:30 +0800

    Working on add to calendar function for RSVP

--- lib/Functions/config.dart
+++ lib/Functions/config.dart
@@ -1,8 +1,21 @@
+//Main url
 final url = 'http://10.0.2.2:3000/';
+
+// User-account related configs
 final register = url + 'users/register';
 final login = url + 'users/login';
 final forget = url + 'users/forgot-password';
 final update = url + 'users/update';
+final logout = url + 'users/logout';
+
+// Event related configs
 final createEvent = url + 'create_event/add';
-final deleteEvent = url + 'create_event/:id';
-// final viewEvent = url + '/view_event';
+final getEvent = url + 'create_event/get';
+final getEventByEmail = url + 'create_event/email/:email';
+final getEventByInviteeEmail = url + 'create_event/invitee/:invitee_email';
+final updateEvent = url + 'create_event/update/:event_code';
+final deleteEvent = url + 'create_event/delete/:event_code';
+
+// RSVP related configs
+final addRSVP = url + 'rsvp/add';
+final getRSVPByStatus = url + 'rsvp/status/:status';
